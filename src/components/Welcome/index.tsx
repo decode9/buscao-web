2d8d85a4ccb52cd788de5741a8d186927a4211e4
free1f <liseth.yggdrasil@gmail.com>
2021-03-17 15:03:33 -0400

    responsive

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,8 +1,26 @@
 import styles from './styles.module.scss'
 import Search from '../Search'
+import { useState, useEffect } from 'react'
 
 const Welcome = ({ section, title = '' }) => {
   const sourceUrl = section?.background?.sourceUrl
+  const sourceUrlResponsive = section?.backgroundResponsive?.sourceUrl
+
+  const [responsive, setResponsive] = useState(false);
+  
+  useEffect(() => {
+    if(window.innerWidth < 576) setResponsive(true);
+    window.addEventListener('resize', checkWidth);
+
+    return () => window.removeEventListener('resize', () => {});
+  }, []);
+
+  const checkWidth = () => {
+    const mediaQuery = window.matchMedia('(max-width: 576px)');
+    if (mediaQuery.matches) return setResponsive(true);
+    setResponsive(false);
+  };
+
   return (
     <>
       <div className={"_main"}>
@@ -45,8 +63,8 @@ const Welcome = ({ section, title = '' }) => {
 
       <style jsx>{`
         ._main {
-          background-image: url(${sourceUrl});
-          background-size: 100% 100%;
+          background-image: url(${responsive ? sourceUrlResponsive : sourceUrl});
+          background-size: ${responsive ? '100% 100%' : 'cover'};
           background-repeat: no-repeat;
           background-position: center;
           height: 80vh;
